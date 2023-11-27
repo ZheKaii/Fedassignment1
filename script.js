5cd99025cfc0ebe84cbbd8d233732bcdba726d6f
ZheKaii <s10258524@connect.np.edu.sg>
2023-11-27 17:29:03 +0800

    updates to account page

--- script.js
+++ script.js
@@ -2,11 +2,61 @@ let signupBtn = document.getElementById("signupBtn");
 let signinBtn = document.getElementById("signinBtn");
 let nameField = document.getElementById("nameField");
 let title1 = document.getElementById("title1");
-let form = document.getElementById("form");
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let password = document.getElementById("password");
+let nameError = document.getElementById('name-error');
+let emailError = document.getElementById('email-error');
+let passwordError = document.getElementById('password-error');
+let submitError = document.getElementById('submit-error');
 
+function validateName() {
+    var name = document.getElementById('name').value;
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if ((name.length > 0) && (name.length < 5)) {
+        nameError.innerHTML = 'Name must be at least 5 characters long';
+        return false;
+    }
+    nameError.innerHTML = '';
+    return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById('email').value;
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z]\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email Invalid'
+        return false;
+    }
+    emailError.innerHTML = '';
+    return true;
+}
+
+function validatePassword() {
+    var password = document.getElementById('password').value;
+    if (password.length == 0) {
+        passwordError.innerHTML = 'Password is required';
+        return false;
+    }
+    if ((password.length > 0) && (password.length < 8)) {
+        passwordError.innerHTML = 'Password must be at least 8 characters long';
+        return false;
+    }
+    passwordError.innerHTML = '';
+    return true;
+}
+
+function validateForm() {
+    if (!validateName() || !validateEmail() || !validatePassword()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error';
+        setTimeout(function(){submitError.style.display = 'none';}, 3000);
+        return false;
+    }
+}
 
 signinBtn.onclick = function(){
     nameField.style.maxHeight = "0";
